f192ad6c36c3e1979153bf996a88e78368b64b3e
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2024-11-17 22:25:42 +0800

    Fix: Modal not closing

--- src/app/components/UI/Dialog.jsx
+++ src/app/components/UI/Dialog.jsx
@@ -1,16 +1,21 @@
 import Button from "./Button";
-
+import { useState } from "react";
 function Dialog() {
+  const [open, setIsOpen] = useState(false);
   return (
-    <div className="fixed inset-0 z-50 ">
-      <div className="flex items-center justify-center absolute bg-black bg-opacity-50 inset-0">
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h2 className="text-xl font-bold mb-4">My Friend AI</h2>
-          <p className="mb-4">Thank You for Waitlisting!</p>
-          <Button onClick={() => setIsOpen(false)}>Close</Button>
+    <>
+      {open && (
+        <div className="fixed inset-0 z-50 ">
+          <div className="flex items-center justify-center absolute bg-black bg-opacity-50 inset-0">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h2 className="text-xl font-bold mb-4">My Friend AI</h2>
+              <p className="mb-4">Thank You for Waitlisting!</p>
+              <Button onClick={() => setIsOpen(false)}>Close</Button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
