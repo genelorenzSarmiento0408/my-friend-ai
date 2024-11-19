f2b723c3cf4bbc55a8006b640a10b67a757eb081
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2024-11-19 20:37:34 +0800

    Fix: Image size in Flagship Product

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -2,26 +2,28 @@ import Image from "next/image";
 import Button from "./UI/Button";
 import About_image from "../assets/About_image.jpg";
 import icon_goals from "../assets/icon_goals.png";
+import placeholder from "@/app/assets/iStock-1995180888.png";
+import icon_explore from "@/app/assets/icon_explore.png";
 import arrow from "@/app/assets/Arrow_right.png";
 import Link from "next/link";
 function About() {
   return (
     <div
-      className="grid justify-center min-h-32 w-full px-6 py-12 lg:py-0 lg:px-0 lg:flex-row gap-4 bg-About bg-cover"
+      className="grid lg:flex-row mt-8 px-6 lg:px-20 py-14 gap-6 bg-About bg-cover bg-no-repeat"
       id="about"
     >
-      <div className="col-span-3 lg:col-span-1 lg:col-end-3 lg:row-end-2 md:mt-12 h-full lg:flex lg:flex-col lg:justify-end lg:ml-6">
+      <div className="col-span-3 lg:col-span-2 lg:col-end-5 lg:row-end-2 h-full lg:flex lg:flex-col lg:justify-end lg:ml-6">
         <Image src={icon_goals} alt="icon" />
         <h1 className="text-3xl font-bold my-6">Flagship Product</h1>
       </div>
-      <div className="col-span-3 md:col-span-4 md:row-span-2 md:col-end-2">
+      <div className="col-span-3 md:col-span-2 md:row-span-2 md:place-self-center md:min-w-full md:h-full">
         <Image
           alt="about image"
           src={About_image}
-          className="rounded-3xl lg:rounded-l-none md:h-full md:object-cover"
+          className="rounded-3xl md:mt-auto md:w-full md:h-full object-cover"
         />
       </div>
-      <div className="w-full h-auto py-6 col-span-4 lg:col-span-2 lg:col-end-4 lg:px-6 lg:pr-12 lg:flex lg:flex-col lg:justify-start">
+      <div className="w-full h-auto py-6 col-span-4 lg:col-span-2 lg:col-end-5 lg:px-6 lg:pr-12 lg:flex lg:flex-col lg:justify-start">
         <p className="text-lg mb-6 font-extralight">
           My-Friend is building Samantha: the world{"'"}s first woman-centered
           AI. With features developed around the unique challenges that women
